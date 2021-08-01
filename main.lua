62f7a6e70ae4e1704e93bac856d876405d9e2fd4
Estrol Mendex <estrol@estrol.space>
2021-08-01 21:51:06 +0700

    add bunch things from original source code

--- main.lua
+++ main.lua
@@ -14,6 +14,7 @@
 -- loadstring, HttpGet, getgc, getloadedmodules, getconnections, and set_thread_identity or whatever you call it
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/uwuware-ui/main/main.lua"))()
+local UserInputService = game:GetService("UserInputService")
 
 local framework, scrollHandler
 while true do
@@ -41,23 +42,11 @@ end
 local runService = game:GetService('RunService')
 local userInputService = game:GetService('UserInputService')
 local client = game:GetService('Players').LocalPlayer;
+local task = task or getrenv().task
 local random = Random.new()
 
 local fastWait, fastSpawn, fireSignal, rollChance do
-	-- https://eryn.io/gist/3db84579866c099cdd5bb2ff37947cec
-	-- bla bla spawn and wait are bad 
-	-- can also use bindables for the fastspawn idc
-
-    function fastWait(t)
-        local d = 0;
-        while d < t do
-            d = d + runService.RenderStepped:wait()
-        end
-    end
-
-    function fastSpawn(f)
-        coroutine.wrap(f)()
-    end
+    fastWait, fastSpawn = task.wait, task.spawn
 	
 	-- updated for script-ware or whatever
 	-- attempted to update for krnl 
@@ -80,6 +69,15 @@ local fastWait, fastSpawn, fireSignal, rollChance do
 	-- its a bit scuffed rn but it works good enough
 
 	function rollChance()
+		if (library.flags.autoPlayerMode == 'Manual') then
+            if (library.flags.sickHeld) then return 'Sick' end
+            if (library.flags.goodHeld) then return 'Good' end
+            if (library.flags.okayHeld) then return 'Ok' end
+            if (library.flags.missHeld) then return 'Bad' end
+
+            return 'Bad' -- incase if it cant find one
+        end
+
 		local chances = {
 			{ type = 'Sick', value = library.flags.sickChance },
 			{ type = 'Good', value = library.flags.goodChance },
@@ -136,56 +134,108 @@ if shared._id then
 	pcall(runService.UnbindFromRenderStep, runService, shared._id)
 end
 
+if not shared._id then
+
+	-- not mine, got it somewhere in v3rmillion and decided to combine it
+	local ya = getrawmetatable(game)
+	local num = ya.__namecall
+	setreadonly(ya, false)
+	
+	ya.__namecall = newcclosure(function(name, ...)
+		local tabs = {...}
+	
+		if library.flags.noPointsLose then
+			if getnamecallmethod() == "InvokeServer" and tostring(name) == "RF" then
+				if tabs[1][3] == "UpdateScore" and tonumber(tabs[2][1]) then
+					tabs[2][1] = 1000 -- max sick score
+				end
+			end
+		end
+	
+		return num(name, unpack(tabs))
+	end)
+	
+	setreadonly(ya, true)
+end
+
 shared._id = game:GetService('HttpService'):GenerateGUID(false)
 runService:BindToRenderStep(shared._id, 1, function()
 	if (not library.flags.autoPlayer) then return end
 
-	for i, arrow in next, framework.UI.ActiveSections do
-		if (arrow.Side == framework.UI.CurrentSide) and (not marked[arrow]) then 
-			local indice = (arrow.Data.Position % 4) -- mod 4 because 5%4 -> 0, 6%4 = 1, etc
-			local position = map[indice]
-			
-			if (position) then
-				local currentTime = framework.SongPlayer.CurrentlyPlaying.TimePosition
-				local distance = (1 - math.abs(arrow.Data.Time - currentTime)) * 100
+	local arrows = {}
+    for _, obj in next, framework.UI.ActiveSections do
+        arrows[#arrows + 1] = obj;
+    end
 
-				if (arrow.Data.Time == 0) then
-				--	print('invisible', tableToString(arrow.Data), i, distance)
-					continue
-				end
+    for idx = 1, #arrows do
+        local arrow = arrows[idx]
+        if type(arrow) ~= 'table' then 
+            continue
+        end
 
-				local hitChance = hitChances[arrow] or rollChance()
-				hitChances[arrow] = hitChance
+        if (arrow.Side == framework.UI.CurrentSide) and (not marked[arrow]) then
+            local indice = (arrow.Data.Position % 4)
+            local position = map[indice]
+            
+            if (position) then
+                local currentTime = framework.SongPlayer.CurrentlyPlaying.TimePosition
+                local distance = (1 - math.abs(arrow.Data.Time - currentTime)) * 100
 
-				-- if (not chanceValues[hitChance]) then warn('invalid chance', hitChance) end
-				if distance >= chanceValues[hitChance] then
-					marked[arrow] = true;
-					fireSignal(scrollHandler, userInputService.InputBegan, { KeyCode = keys[position], UserInputType = Enum.UserInputType.Keyboard }, false)
+                if (arrow.Data.Time == 0) then
+                    continue
+                end
 
-					-- wait depending on the arrows length so the animation can play
-					if arrow.Data.Length > 0 then
-						fastWait(arrow.Data.Length)
-					else
-						fastWait(0.075) -- 0.1 seems to make it miss more, this should be fine enough?
-					end
+                local hitChance = nil
+                if library.flags.autoPlayerMode == 'Manual' then
+                    hitChance = rollChance()
+                else
+                    hitChance = hitChances[arrow] or rollChance()
+                    hitChances[arrow] = hitChance
+                end
 
-					fireSignal(scrollHandler, userInputService.InputEnded, { KeyCode = keys[position], UserInputType = Enum.UserInputType.Keyboard }, false)
-					marked[arrow] = false;
-				end
-			end
-		end
-	end
+                if distance >= chanceValues[hitChance] then
+                    marked[arrow] = true;
+                    fireSignal(scrollHandler, userInputService.InputBegan, { KeyCode = keys[position], UserInputType = Enum.UserInputType.Keyboard }, false)
+
+                    if arrow.Data.Length > 0 then
+                        -- wait depending on the arrows length so the animation can play
+                        fastWait(arrow.Data.Length)
+                    else
+                        -- 0.1 seems to make it miss more, this should be fine enough?
+                        fastWait(0.075) 
+                    end
+
+                    fireSignal(scrollHandler, userInputService.InputEnded, { KeyCode = keys[position], UserInputType = Enum.UserInputType.Keyboard }, false)
+                    marked[arrow] = false;
+                end
+            end
+        end
+    end
 end)
 
 local window = library:CreateWindow('Funky Friday') do
 	local folder = window:AddFolder('Main') do
 		folder:AddToggle({ text = 'Autoplayer', flag = 'autoPlayer' })
+		folder:AddToggle({ text = 'No points lose', flag = 'noPointsLose' })
+		folder:AddList({ text = 'Autoplayer mode', flag = 'autoPlayerMode', values = { 'Chances', 'Manual' } })
 
 		folder:AddSlider({ text = 'Sick %', flag = 'sickChance', min = 0, max = 100, value = 100 })
 		folder:AddSlider({ text = 'Good %', flag = 'goodChance', min = 0, max = 100, value = 0 })
 		folder:AddSlider({ text = 'Ok %', flag = 'okChance', min = 0, max = 100, value = 0 })
 		folder:AddSlider({ text = 'Bad %', flag = 'badChance', min = 0, max = 100, value = 0 })
 	end
+
+	local folder = window:AddFolder('Keybinds') do
+        folder:AddBind({ text = 'Sick', flag = 'sickBind', key = Enum.KeyCode.One, hold = true, callback = function(val) library.flags.sickHeld = (not val) end, })
+        folder:AddBind({ text = 'Good', flag = 'goodBind', key = Enum.KeyCode.Two, hold = true, callback = function(val) library.flags.goodHeld = (not val) end, })
+        folder:AddBind({ text = 'Ok', flag = 'okBind', key = Enum.KeyCode.Three, hold = true, callback = function(val) library.flags.okayHeld = (not val) end, })
+        folder:AddBind({ text = 'Bad', flag = 'badBind', key = Enum.KeyCode.Four, hold = true, callback = function(val) library.flags.missHeld = (not val) end, })
+    end
+
+    local folder = window:AddFolder("Help") do
+        folder:AddLabel({ text = 'Press F4 to reset table cache (maybe clearing lag)' })
+        folder:AddLabel({ text = 'Press F5 to hide the GUI' })
+    end
 
 	local folder = window:AddFolder('Credits') do
 		folder:AddLabel({ text = 'Credits' })
@@ -194,5 +244,18 @@ local window = library:CreateWindow('Funky Friday') do
 	end
 end
 
+local function HandleInputKeyCode(input, processed)
+    if input.UserInputType == Enum.UserInputType.Keyboard then
+        if input.KeyCode == Enum.KeyCode.F4 and not processed then
+            marked = {} -- Attempt to clear the lag after playing map
+            hitChances = {}
+        else if input.KeyCode == Enum.KeyCode.F5 and not processed then
+            library:Close() -- Dunno why it called Close() not just called Toggle()
+            end
+        end
+    end
+end
+
+UserInputService.InputBegan:Connect(HandleInputKeyCode)
 
 library:Init()
